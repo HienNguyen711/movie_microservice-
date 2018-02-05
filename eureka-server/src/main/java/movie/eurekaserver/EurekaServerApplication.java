0af379e0d2a4dc4513c2fc598c77f0578a908bf5
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-02-05 19:48:22 +0200

    add config server

--- eureka-server/src/main/java/movie/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/movie/eurekaserver/EurekaServerApplication.java
@@ -2,13 +2,19 @@ package movie.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
+
 }
